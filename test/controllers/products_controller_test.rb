45dbaca5a19dc1d9dbd0dda125de8005741ba9f4
jorgegoco <jorgegoco70@gmail.com>
2023-07-25 23:54:21 +0200

    Add new and create for products

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -16,4 +16,17 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_select '.description', 'New PS4'
     assert_select '.price', '300$'
   end
+
+  test "should get new" do
+    get new_product_path
+
+    assert_response :success
+    assert_select 'form' 
+  end
+
+  test "should create product" do
+    post products_path, params: { product: { title: 'Xbox', description: 'New Xbox', price: '275' } }
+
+    assert_redirected_to products_path
+  end
 end
