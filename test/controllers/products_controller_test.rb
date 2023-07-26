f82b78a1de14dbfdc249b3460662480142a8ecb5
jorgegoco <jorgegoco70@gmail.com>
2023-07-26 10:57:28 +0200

    Add validations to Product model, and table

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get products_path
 
     assert_response :success
     assert_select '.product', 2 
   end
 
-  test "should get show" do
+  test 'should get show' do
     get product_path(products(:PS4))
 
     assert_response :success
@@ -17,16 +17,22 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_select '.price', '300$'
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_product_path
 
     assert_response :success
     assert_select 'form' 
   end
 
-  test "should create product" do
+  test 'should create product' do
     post products_path, params: { product: { title: 'Xbox', description: 'New Xbox', price: '275' } }
 
     assert_redirected_to products_path
+  end
+
+  test 'should not create product with empty fields' do
+    post products_path, params: { product: { title: '', description: 'New Xbox', price: '275' } }
+
+    assert_response :unprocessable_entity
   end
 end
