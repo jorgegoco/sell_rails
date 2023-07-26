bba21770f8e4aa7b4dcee9452805d66643469e02
jorgegoco <jorgegoco70@gmail.com>
2023-07-26 20:28:32 +0200

    Add Category model, views, tests, and controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,49 @@
+class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def edit
+    category
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to categories_url, notice: "Category was successfully created." 
+    else
+      render :new, status: :unprocessable_entity 
+    end
+  end
+
+  def update
+    if category.update(category_params)
+      redirect_to categories_url, notice: "Category was successfully updated." 
+    else
+      render :edit, status: :unprocessable_entity 
+    end
+  end
+
+  def destroy
+    category.destroy
+
+    respond_to do |format|
+      format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
+    end
+  end
+
+  private
+    
+    def category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
+end
