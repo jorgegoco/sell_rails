700be8f921eaf4fbbaeba3b79c8d1b3aa9aef536
jorgegoco <jorgegoco70@gmail.com>
2023-07-29 18:50:20 +0200

    Add form to sessions, and controller

---
+++ app/controllers/authentication/sessions_controller.rb
@@ -0,0 +1,15 @@
+class Authentication::SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by('email = :login OR username = :login', { login: params[:login] })
+
+    if @user&.authenticate(params[:password])
+
+      redirect_to products_path, notice: 'Logged in!'
+    else
+      redirect_to new_session_path, alert: 'Invalid login!'
+    end
+  end
+end
