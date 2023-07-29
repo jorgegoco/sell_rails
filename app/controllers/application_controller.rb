656f6180419d79b09544bc05e28a42d50ffee080
jorgegoco <jorgegoco70@gmail.com>
2023-07-29 19:15:16 +0200

    Setup session for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  before_action :set_current_user
+
+  private
+
+  def set_current_user
+    Current.user = User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
--- app/controllers/authentication/sessions_controller.rb
+++ app/controllers/authentication/sessions_controller.rb
@@ -6,7 +6,7 @@ class Authentication::SessionsController < ApplicationController
     @user = User.find_by('email = :login OR username = :login', { login: params[:login] })
 
     if @user&.authenticate(params[:password])
-
+      session[:user_id] = @user.id
       redirect_to products_path, notice: 'Logged in!'
     else
       redirect_to new_session_path, alert: 'Invalid login!'
