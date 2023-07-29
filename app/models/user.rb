9ecf501db4a94278e6974668592af3862ac10a03
jorgegoco <jorgegoco70@gmail.com>
2023-07-29 14:42:17 +0200

    Add more validations for user form, and add create User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
-  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true,
+  format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "must be a valid email address" }
+  validates :username, presence: true, uniqueness: true,
+  length: { in: 3..15 }, format: { with: /\A[a-zA-Z0-9]+\Z/, message: "only allows letters and numbers" }
   validates :password, length: { minimum: 6}
+
+  before_save :downcase_attributes
+
+  private
+
+  def downcase_attributes
+    self.username = username.downcase
+    self.email = email.downcase
+  end
 end
