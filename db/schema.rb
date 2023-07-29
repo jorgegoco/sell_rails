10d29f0bd6191bb1d1aea1cc955ed4d797e987cc
jorgegoco <jorgegoco70@gmail.com>
2023-07-29 13:11:54 +0200

    Clean up code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_27_120435) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_101852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_27_120435) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
