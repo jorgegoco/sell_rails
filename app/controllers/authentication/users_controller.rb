538cd344de846a4b2c8321fe4f1c94a5edd95775
jorgegoco <jorgegoco70@gmail.com>
2023-07-29 13:39:08 +0200

    Add User controller with authentication namespace

---
+++ app/controllers/authentication/users_controller.rb
@@ -0,0 +1,9 @@
+class Authentication::UsersController < ApplicationController
+  def new
+
+  end
+
+  def create
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :categories, except: :show
   resources :products, path: '/'
+
+  namespace :authentications do
+    resources :users, only: %i[new create]
+  end
 end
