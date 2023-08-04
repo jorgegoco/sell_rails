b9d751893d6190660d18203c78eb5c3308050e26
jorgegoco <jorgegoco70@gmail.com>
2023-08-04 11:40:31 +0200

    Transfer the categories layout in index product to the new component

--- app/components/category_component.rb
+++ app/components/category_component.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 class CategoryComponent < ViewComponent::Base
-  def initialize(category:)
+  def initialize(category: nil)
     @category = category
   end
 
+  def title
+    @category ? @category.name : 'All'
+  end
+
+  def link
+    @category ? products_path(category_id: @category.id) : products_path
+  end
+
+  def classes
+    'bg-white text-gray-600 px-4 py-2 rounded-2xl drop-shadow-sm hover:bg-gray-300'
+  end
 end
