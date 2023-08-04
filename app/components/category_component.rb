ef857ed488158f5d5cf675b6f5a9b61c1fa99ffe
jorgegoco <jorgegoco70@gmail.com>
2023-08-04 12:29:29 +0200

    Apply tailwind classes to categories in index product

--- app/components/category_component.rb
+++ app/components/category_component.rb
@@ -13,7 +13,17 @@ class CategoryComponent < ViewComponent::Base
     @category ? products_path(category_id: @category.id) : products_path
   end
 
+  def active?
+    return true if !@category && !params[:category_id]
+
+    @category&.id == params[:category_id].to_i
+  end
+
+  def background 
+    active? ? 'bg-gray-300' : 'bg-white' 
+  end
+
   def classes
-    'bg-white text-gray-600 px-4 py-2 rounded-2xl drop-shadow-sm hover:bg-gray-300'
+    "text-gray-600 px-4 py-2 rounded-2xl drop-shadow-sm hover:bg-gray-300 #{background}"
   end
 end
