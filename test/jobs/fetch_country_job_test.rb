b20f450c5e485901d548fd364ebafd970705346d
jorgegoco <jorgegoco70@gmail.com>
2023-08-06 20:17:52 +0200

    Add test to job

--- test/jobs/fetch_country_job_test.rb
+++ test/jobs/fetch_country_job_test.rb
@@ -1,7 +1,9 @@
-require "test_helper"
+require 'test_helper'
 
 class FetchCountryJobTest < ActiveJob::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'enqueue job' do
+    assert_enqueued_jobs 0
+    FetchCountryJob.perform_later('1', '0.0.0.0')
+    assert_enqueued_jobs 1
+  end
 end
