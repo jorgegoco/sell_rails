a402d9b31af8d3d755e095cef26f176457c4b312
jorgegoco <jorgegoco70@gmail.com>
2023-08-06 20:12:42 +0200

    Add sidekiq gem to do background jobs in redis

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module SellRails
 
     # Allow multiquery
     config.active_record.async_query_executor = :global_thread_pool 
+
+    # Background jobs
+    config.active_job.queue_adapter = :sidekiq
   end
 end
