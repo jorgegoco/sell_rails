9caab26c30f6c6cf9a6f6fc9307506233a24ec7a
jorgegoco <jorgegoco70@gmail.com>
2023-08-09 19:07:19 +0200

    Change user fixtures file and fix tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,6 +12,6 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   def login
-    post sessions_path, params: { login: 'mary@gmail.com', password: 'testme' }
+    post sessions_path, params: { login: 'mary@sellit.com', password: 'testme' }
   end
 end
